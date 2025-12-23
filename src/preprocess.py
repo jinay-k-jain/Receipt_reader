189d207da49aaf79961210d1b26756b0c7009816
Jinay Jain <Jinayjain56@gmail.com>
2025-12-23 11:11:46 +0530

    Refactor image preprocessing code

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,16 +3,11 @@ import os
 
 IMG_PATH = "data/raw_images/lic_01.jpeg"
 OUT_DIR = "data/processed_images"
-
 os.makedirs(OUT_DIR, exist_ok=True)
-
 img = cv2.imread(IMG_PATH)
 if img is None:
     raise FileNotFoundError("Image not found. Check path.")
-
-# --- Preprocessing ---
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 th = cv2.adaptiveThreshold(
     gray,
     255,
@@ -21,9 +16,6 @@ th = cv2.adaptiveThreshold(
     31,
     5
 )
-
-# Save outputs for inspection
 cv2.imwrite(f"{OUT_DIR}/gray.jpg", gray)
 cv2.imwrite(f"{OUT_DIR}/threshold.jpg", th)
-
 print("Preprocessing complete. Check data/processed_images/")
